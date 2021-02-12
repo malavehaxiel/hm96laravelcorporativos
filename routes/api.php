56d840e8677730c347f3ae216207561dd2056d6e
Haxiel Malave <malavehaxiel96@gmail.com>
2021-02-12 00:25:35 -0400

    Instalación de passport

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['prefix' => 'auth'], function () {
+
+    Route::post('login', 'Auth\AuthController@login')->name('login');
+    Route::post('signup', 'Auth\AuthController@signUp');
+
+    Route::group(['middleware' => 'auth:api'], function() {
+
+        Route::get('logout', 'Auth\AuthController@logout');
+        Route::get('user', 'Auth\AuthController@user');
+    });
 });
